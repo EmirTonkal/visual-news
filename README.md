ace05f3b6e9a55c215c05ca464e258aeab6a90b2
cello97 <emirtonkal97@gmail.com>
2021-09-15 11:56:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# vue-rss-espn
+
+## Project overview
+I built this project to learn Vue development.<br/>
+It accesses the ESPN NFL RSS feeds using a proxy 
+to translate xml to json.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+	<div id="app">
+		<h1>Visual News</h1>
+		<DivisionSelect v-model="selected"></DivisionSelect>
+		<Loading :loaded="loaded"></Loading>
+		<!-- Create an Item component for each entry in the RSS feed -->
+		<Item
+			v-for="(item, index) in items"
+			:key="index"
+			:title="item.title"
+			:link="item.link"
+			:image="item.image"
+			:desc="item.description"
+		>
+		</Item>
+	</div>
+</template>
+
+<script>
+import Item from "./components/Item.vue";
+import DivisionSelect from "./components/DivisionSelect.vue";
+import Loading from "./components/Loading.vue";
+import axios from "axios";
+
+export default {
+	name: "app",
+	data() {
+		return {
+			selected: "afcsouth",
+			items: null,
+			loaded: false,
+		};
+	},
+	components: { Item, DivisionSelect, Loading },
+	watch: {
+		selected: function () {
+			this.getData();
+		},
+	},
+	methods: {
+		/*
+      Call back-end to transform XML RSS feed to JSON & bypass CORS restrictions.
+    */
+		getData: function () {
+			axios
+				.get("https://flannel-glade.glitch.me", {
+					params: {
+						rss: "http://www.espn.com/blog/feed?blog=" + this.selected,
+					},
+				})
+				.then((response) => {
+					this.items = response.data.rss.channel.item;
+					this.loaded = true;
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		},
+	},
+	mounted() {
+		this.getData();
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin: 0 auto;
+	padding: 2% 0;
+	max-width: 800px;
+}
+
+h1 {
+	font-size: 1.5em;
+}
+</style>
